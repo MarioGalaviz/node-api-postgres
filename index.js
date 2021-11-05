b8bcf3ee050472b7f58eaefa5c7a10657e7baee9
Mario <mga.375@gmail.com>
2021-11-05 11:20:26 -0600

    mejor no cambioa nterior

--- index.js
+++ index.js
@@ -32,19 +32,6 @@ const addBook = (request, response) => {
   )
 }
 
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-  
-  app.use((err, req, res, next) => {
-    res.locals.error = err;
-    const status = err.status || 500;
-    res.status(status);
-    res.render('error');
-  });
-
 app
   .route('/books')
   // GET endpoint
